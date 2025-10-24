8c94bafdff45064ffbd821e1e7a894eb75428d97
availe <94802525+availe@users.noreply.github.com>
2025-10-23 23:37:26 -0400

    Update App.kt

--- composeApp/src/commonMain/kotlin/io/availe/unstyledsamples/App.kt
+++ composeApp/src/commonMain/kotlin/io/availe/unstyledsamples/App.kt
@@ -1,6 +1,14 @@
 package io.availe.unstyledsamples
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.composeunstyled.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -11,11 +19,13 @@ fun App() {
 
     val state = rememberTabGroupState(categories.first(), orderedTabs = categories)
 
-    TabGroup(state = state) {
-        TabList {
+    TabGroup(state = state, Modifier.fillMaxSize().padding(16.dp)) {
+        TabList(Modifier.fillMaxWidth()) {
             categories.forEach { category ->
-                Tab(key = category) {
-                    Text(category)
+                Box(Modifier.padding(4.dp).background(Color.LightGray)) {
+                    Tab(key = category) {
+                        Text(category)
+                    }
                 }
             }
         }
