a6363af0504483ac0e028df30ad56608a73d73ba
availe <94802525+availe@users.noreply.github.com>
2025-10-23 23:30:06 -0400

    Update App.kt

--- composeApp/src/commonMain/kotlin/io/availe/unstyledsamples/App.kt
+++ composeApp/src/commonMain/kotlin/io/availe/unstyledsamples/App.kt
@@ -1,10 +1,29 @@
 package io.availe.unstyledsamples
 
 import androidx.compose.runtime.Composable
+import com.composeunstyled.*
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    val categories = listOf("Tab1", "Tab2", "Tab3", "Tab4", "Tab5")
 
+    val state = rememberTabGroupState(categories.first(), orderedTabs = categories)
+
+    TabGroup(state = state) {
+        TabList {
+            categories.forEach { category ->
+                Tab(key = category) {
+                    Text(category)
+                }
+            }
+        }
+
+        categories.forEach { category ->
+            TabPanel(key = category) {
+                Text("$category Content")
+            }
+        }
+    }
 }
